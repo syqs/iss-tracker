af59ae9ba88849b516e5d380ae1822f530b31cb0
syqs <kristian.magda@gmail.com>
2016-10-22 18:15:38 -0500

    initial commit

--- client/controllers/mapCtrl.js
+++ client/controllers/mapCtrl.js
@@ -4,99 +4,139 @@ angular.module('myApp')
 //Map controller
 .controller('MapController', function($scope, $timeout, $http) {
 
-  $scope.map;
-  $scope.iss = {};
-  $scope.tweets = [];
-  var counter = 0;
+  var scene, camera, renderer;
+  var geometry, material, mesh;
 
-  // Initialization  
-  $scope.$on('$viewContentLoaded', function() {
-    $scope.getIssLoc();  
-    $scope.getTwitterFeeds();
-  })
+  init();
+  animate();
 
-  // Callback for seting the map to the state
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
+  function init() {
 
-  // Get twitter data 
-  $scope.getTwitterFeeds = function() {
+      scene = new THREE.Scene();
 
-    // Initiate a call to backend
-    $http.get("/api")
-      .then(function(feeds) {
-        $scope.tweets = feeds.data.statuses
-      })
-      .catch(function(err) {
-        console.log('error: ', err)
-      })
+      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+      camera.position.z = 1000;
+
+      geometry = new THREE.SphereGeometry( 200 );
+      material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+      mesh = new THREE.Mesh( geometry, material );
+      scene.add( mesh );
+
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+
+      document.body.appendChild( renderer.domElement );
+
   }
 
-  // Draw the map 
-  $scope.drawMap = function(){
-    var lat;
-    var lng;
-    if ($scope.iss.location){
-      lat = $scope.iss.location.latitude;
-      lng = $scope.iss.location.longitude;
-    }else{
-      lat =  -31.54961366315104;
-      lng = -105.60398630491167;
-    }
-    var latlng = new google.maps.LatLng(lat, lng);
-    var myOptions = {
-      zoom: 2,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    $scope.map = new google.maps.Map(document.getElementById("map"), myOptions);
+  function animate() {
+
+      requestAnimationFrame( animate );
+
+      mesh.rotation.x += 0.0;
+      mesh.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+
   }
 
-  // Get the location of ISS
-  $scope.getIssLoc = function() {
-    if(counter<2){
-      $scope.drawMap();
-      counter++
-    }
 
-    // Initiate a call to back end
-    $http.get("/issLocation")
-      .then(function(location) {
-        $scope.iss.location = location.data.iss_position;
+
+
+  // $scope.map;
+  // $scope.iss = {};
+  // $scope.tweets = [];
+  // var counter = 0;
+
+  // // Initialization  
+  // $scope.$on('$viewContentLoaded', function() {
+  //   $scope.getIssLoc();  
+  //   $scope.getTwitterFeeds();
+  // })
+
+  // // Callback for seting the map to the state
+  // $scope.mapCreated = function(map) {
+  //   $scope.map = map;
+  // };
+
+  // // Get twitter data 
+  // $scope.getTwitterFeeds = function() {
+
+  //   // Initiate a call to backend
+  //   $http.get("/api")
+  //     .then(function(feeds) {
+  //       $scope.tweets = feeds.data.statuses
+  //     })
+  //     .catch(function(err) {
+  //       console.log('error: ', err)
+  //     })
+  // }
+
+  // // Draw the map 
+  // $scope.drawMap = function(){
+  //   var lat;
+  //   var lng;
+  //   if ($scope.iss.location){
+  //     lat = $scope.iss.location.latitude;
+  //     lng = $scope.iss.location.longitude;
+  //   }else{
+  //     lat =  -31.54961366315104;
+  //     lng = -105.60398630491167;
+  //   }
+  //   var latlng = new google.maps.LatLng(lat, lng);
+  //   var myOptions = {
+  //     zoom: 2,
+  //     center: latlng,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //   $scope.map = new google.maps.Map(document.getElementById("map"), myOptions);
+  // }
+
+  // // Get the location of ISS
+  // $scope.getIssLoc = function() {
+  //   if(counter<2){
+  //     $scope.drawMap();
+  //     counter++
+  //   }
+
+  //   // Initiate a call to back end
+  //   $http.get("/issLocation")
+  //     .then(function(location) {
+  //       $scope.iss.location = location.data.iss_position;
         
-        // Create a marker with latest position
-        $scope.addMarker({
-          lat: location.data.iss_position.latitude,
-          lng: location.data.iss_position.longitude
-        })
-      })
-      .catch(function(err) {
-        console.log('error: ', err)
-      })
-  }
+  //       // Create a marker with latest position
+  //       $scope.addMarker({
+  //         lat: location.data.iss_position.latitude,
+  //         lng: location.data.iss_position.longitude
+  //       })
+  //     })
+  //     .catch(function(err) {
+  //       console.log('error: ', err)
+  //     })
+  // }
 
-  //Add single Marker
-  $scope.addMarker = function(pos) {
-    var icons = 'images/iss-ico.png'
-    var marker = new google.maps.Marker({
-      position: {
-        lat: pos.lat,
-        lng: pos.lng
-      },
-      icon: icons
-    });
-    // Put marker on the map
-    marker.setMap($scope.map);
-  }
+  // //Add single Marker
+  // $scope.addMarker = function(pos) {
+  //   var icons = 'images/iss-ico.png'
+  //   var marker = new google.maps.Marker({
+  //     position: {
+  //       lat: pos.lat,
+  //       lng: pos.lng
+  //     },
+  //     icon: icons
+  //   });
+  //   // Put marker on the map
+  //   marker.setMap($scope.map);
+  // }
 
-  // Run these periodically
-  setInterval(function() {
-    $scope.getIssLoc();
-  }, 4000)
+  // // Run these periodically
+  // setInterval(function() {
+  //   $scope.getIssLoc();
+  // }, 4000)
 
-  setInterval(function() {
-    $scope.getTwitterFeeds();
-  }, 30000)
+  // setInterval(function() {
+  //   $scope.getTwitterFeeds();
+  // }, 30000)
 
 });
